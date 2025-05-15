386c5f4417d03f7c4ea9a60c0fe5b08bf176390f
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2025-05-14 21:16:42 -0300

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import './App.css';
 import AgregarUsuario from './AgregarUsuario';
 import EditarUsuario from './EditarUsuario';
 import ListaUsuarios from './ListaUsuarios';
-import {BrowserRouter, Routes, route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>MERN CRUD CON REACT</h1>
-        <ListaUsuarios/>
-        <AgregarUsuario/>
-        <EditarUsuario/>
-
+        <BrowserRouter>
+        <Routes>
+           <Route path = '/' element={<ListaUsuarios/>} exact> </Route>
+           <Route path = '/agregarusuario' element={<AgregarUsuario/>} exact> </Route>
+           <Route path = '/editarusuario' element={<EditarUsuario/>} exact> </Route>
+        </Routes>
+        </BrowserRouter>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           M <code>Mongodb</code> E <code>Express.js</code> R <code>React</code> N <code>Node.js</code>.
