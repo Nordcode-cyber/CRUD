91a401eeaad3654af507448a9a0c53ce28f22cca
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2025-05-17 23:43:34 -0300

    parsing

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
+       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
     <a class="navbar-brand" href="#">CRUD MERN</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -20,7 +20,10 @@ function App() {
           <a class="nav-link active" aria-current="page" href="#">Inicio</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="agregarusuario">AgregarUsuario</a>
+          <a class="nav-link active " aria-current="page" href="agregarusuario">AgregarUsuario</a>
+        </li>
+          <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">ListaUsuarios</a>
         </li>
         <li class="nav-item dropdown">
           
