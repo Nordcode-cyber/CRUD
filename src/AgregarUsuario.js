9b43241458abf503638146e1139bb1007c59821a
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2025-05-21 21:33:08 -0300

    parsing Agregar usuario

--- src/AgregarUsuario.js
+++ src/AgregarUsuario.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 // (>) y "menor que" (<>)
 function AgregarUsuario(){
+    //hooks
+    const [nombre, setNombre]=useState('')
+    const [email, setEmail]=useState('')
+    const [telefono, setTelefono]=useState('')
+
     function agregarUsuario(){
+        var usuario ={
+        nombre: nombre,
+        email:email,
+        telefono:telefono
+        }
+
         
     }
 
@@ -16,16 +27,16 @@ function AgregarUsuario(){
              <div className="col-sm-6 offset-3">
                 <div className="mb-3">
                     <label htmlFor="nombre" className="form-label">Nombre</label>
-                    <input type="text" className="form-control"></input>
+                    <input type="text" className="form-control" value={nombre}onChange={(e)=>{setNombre(e.target.value)}}></input>
                 </div>
 
                 <div className="mb-3">
                     <label htmlFor="email" className="form-label">Email</label>
-                    <input type="email" className="form-control"></input>
+                    <input type="email" className="form-control"  value={email}onChange={(e)=>{setEmail(e.target.value)}}></input>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="telefono" className="form-label">Telefono</label>
-                    <input type="text" className="form-control"></input>
+                    <input type="text" className="form-control" value={telefono}onChange={(e)=>{setTelefono(e.target.value)}}></input>
                 </div>
 
                 <button onClick= {AgregarUsuario} className="btn-btn-success">Guardar Usuario</button>
